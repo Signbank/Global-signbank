c38bdc6d33d1cf3a110c88cbb4317a6e324c5419
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-05-01 10:59:17 +0200

    Turned off existing, seemingly unused, user group check #30

--- signbank/pages/views.py
+++ signbank/pages/views.py
@@ -42,11 +42,13 @@ def page(request, url='/'):
     
     # If registration is required for accessing this page, and the user isn't
     # logged in, redirect to the login page.
-    if f.group_required:
-        if not request.user.is_authenticated() :
-            from django.contrib.auth.views import redirect_to_login
-            return redirect_to_login(request.path)
-    
+
+    # if len(f.group_required.all()) > 0:
+    #
+    #      if not request.user.is_authenticated() :
+    #          from django.contrib.auth.views import redirect_to_login
+    #          return redirect_to_login(request.path)
+
     if f.template_name:
         t = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
     else:
