28af5de6e557a066cc44724212020d3c4325f0ec
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-12-02 17:51:26 +0100

    Started with front end for adding other videos #47

--- media/js/gloss_edit.js
+++ media/js/gloss_edit.js
@@ -80,6 +80,7 @@ function disable_edit() {
     $('#add_relation_form').hide();
     $('#add_relationtoforeignsign_form').hide();
     $('#add_morphologydefinition_form').hide();
+    $('#add_other_video').hide();
     $('.definition_delete').hide();
     $('.relation_delete').hide();
     $('.relationtoforeignsign_delete').hide();
@@ -98,6 +99,7 @@ function enable_edit() {
     $('#add_relation_form').show();
     $('#add_relationtoforeignsign_form').show();
     $('#add_morphologydefinition_form').show();
+    $('#add_other_video').show();
     $('.definition_delete').show();
     $('.relation_delete').show();
     $('.relationtoforeignsign_delete').show();
