9a8b328e7a1d6c003c9c12c575edaf17aa20d25c
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-02-19 14:41:02 +0100

    Update to the format of the server specific settings file #124 #134 #114

--- signbank/settings/server_specific.py
+++ signbank/settings/server_specific.py
@@ -1,3 +1,23 @@
+import os
+import socket
+
+hostname = socket.gethostname()
+
+#This should be cleaned up
+if hostname == 'spitfire':
+    ROOT = '/var/www2/signbank/live/'
+    BASE_DIR = ROOT+'repo/signbank/'
+    WRITABLE_FOLDER = ROOT+'writable/'
+elif hostname == 'applejack':
+    ROOT = '/scratch2/www/ASL-signbank/'
+    BASE_DIR = ROOT+'repo/NGT-signbank/'
+    WRITABLE_FOLDER = ROOT+'writable/'
+else:
+    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+#Influences which template and css folder are used
+SIGNBANK_VERSION_CODE = '' #'ASL'
+
 URL = '' #'/asl-signbank'
 
 FIELDS = {}
