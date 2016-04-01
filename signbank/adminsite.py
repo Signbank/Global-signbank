fba2ac6901c5451a39068c0ee1463ed6a725e3a2
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-04-01 14:37:45 +0200

    Extended the django admin user list view #106

--- signbank/adminsite.py
+++ signbank/adminsite.py
@@ -8,3 +8,14 @@ from pages.admin import PageAdmin
 publisher_admin = AdminSite('pageadmin')
 publisher_admin.register(Page, PageAdmin)
 publisher_admin.register(User, UserAdmin)
+
+def groups_part_of(self,obj):
+    return ', '.join([group.name for group in obj.groups.all()])
+
+def number_of_logins(self,obj):
+    return obj.get_profile().number_of_logins
+
+UserAdmin.groups_part_of = groups_part_of
+UserAdmin.number_of_logins = number_of_logins
+
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'last_login', 'groups_part_of', 'number_of_logins')
