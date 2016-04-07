1098a5fa2d9d4dfc9aa828f201d062e74c1c3cc6
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-04-07 09:52:26 +0200

    Added expiry date to django admin user list #40

--- signbank/adminsite.py
+++ signbank/adminsite.py
@@ -15,7 +15,11 @@ def groups_part_of(self,obj):
 def number_of_logins(self,obj):
     return obj.get_profile().number_of_logins
 
+def expiry_date(self,obj):
+    return obj.get_profile().expiry_date
+
 UserAdmin.groups_part_of = groups_part_of
 UserAdmin.number_of_logins = number_of_logins
+UserAdmin.expiry_date = expiry_date
 
-UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'last_login', 'groups_part_of', 'number_of_logins')
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'last_login', 'groups_part_of', 'number_of_logins','expiry_date')
