af2b2f900c029e7144c5ad7a34b61db0aa68ac3d
ErwinKomen <E.Komen@ru.nl>
2016-06-02 09:22:03 +0200

    Changed format of code and comments to satisfy the standards

--- signbank/dictionary/templatetags/wrong_sign.py
+++ signbank/dictionary/templatetags/wrong_sign.py
@@ -4,20 +4,18 @@ register = Library()
 sWrongSignTag = "video:_wrong_sign"     # This is the Signbank-implementation (NGT, ASL) dependent part!
 
 @register.filter
-# --------------------------------------------------------------------------------------------
-# Name :    wrong_sign
-# Goal :    Check if this instance has a tag indicating that it is a wrong sign
-#           This tag is defined in the database and is Signbank-implementation dependent
-#           The particular value used by a signbank must be specified in [sWrongSignTag] above
-# History:
-# 30/may/2016   ERK Created
-# --------------------------------------------------------------------------------------------
 def wrong_sign(qs):
+    """Check if instance @qs has a tag indicating that it is a wrong sign.
+
+    The 'wrong-sign-tag' is defined in the database and is Signbank-implementation dependent;
+    The particular value used by a signbank must be specified in [sWrongSignTag] above
+    """
+
     # Initialise boolean return
     bHasWrongSign = False
     # Walk the query-set containing all the tag-objects passed on
     for item in qs:
         # Check if the .name part matches the text of the expected wrong sign tag
-        if (item.name == sWrongSignTag):
+        if item.name == sWrongSignTag:
             bHasWrongSign = True
     return bHasWrongSign
