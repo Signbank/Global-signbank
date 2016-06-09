5d1ad397d43e6506501bce027040e8b02de461ff
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-06-09 14:22:15 +0200

    Remove semicolons (from my python semicolon phase) :)

--- signbank/tools.py
+++ signbank/tools.py
@@ -20,33 +20,33 @@ def video_to_signbank(source_folder,gloss,extension):
         
     #Add a dot before the extension if needed
     if extension[0] != '.':
-        extension = '.' + extension;
+        extension = '.' + extension
 
     #Figure out some names
-    annotation_id = gloss.annotation_idgloss;
-    pk = str(gloss.pk);
-    destination_folder = SB_VIDEO_FOLDER+annotation_id[:2]+'/';
+    annotation_id = gloss.annotation_idgloss
+    pk = str(gloss.pk)
+    destination_folder = SB_VIDEO_FOLDER+annotation_id[:2]+'/'
 
     #Create the necessary subfolder if needed
     if not os.path.isdir(destination_folder):
-        os.mkdir(destination_folder);
+        os.mkdir(destination_folder)
 
     #Move the file
-    source = source_folder+annotation_id+extension;
-    goal = destination_folder+annotation_id+'-'+pk+extension;
+    source = source_folder+annotation_id+extension
+    goal = destination_folder+annotation_id+'-'+pk+extension
 
     if os.path.isfile(goal):
-        overwritten = True;
+        overwritten = True
     else:
-        overwritten = False;
+        overwritten = False
 
     try:
-        shutil.copy(source,goal);
-        was_allowed = True;
+        shutil.copy(source,goal)
+        was_allowed = True
     except IOError:
-        was_allowed = False;
+        was_allowed = False
 
-    return overwritten,was_allowed;
+    return overwritten,was_allowed
 
 def unescape(string):
 
@@ -60,9 +60,9 @@ def compare_valuedict_to_gloss(valuedict,gloss):
 
     #Create an overview of all fields, sorted by their human name
     with override(LANGUAGE_CODE):
-        fields = {field.verbose_name: field for field in gloss._meta.fields};
+        fields = {field.verbose_name: field for field in gloss._meta.fields}
 
-        differences = [];
+        differences = []
 
         #Go through all values in the value dict, looking for differences with the gloss
         for human_key, new_human_value in valuedict.items():
@@ -72,7 +72,7 @@ def compare_valuedict_to_gloss(valuedict,gloss):
             #If these are not fields, but relations to other parts of the database, go look for differenes elsewhere
             if human_key == 'Keywords':
 
-                current_keyword_string = str(', '.join([translation.translation.text.encode('utf-8') for translation in gloss.translation_set.all()]));
+                current_keyword_string = str(', '.join([translation.translation.text.encode('utf-8') for translation in gloss.translation_set.all()]))
 
                 if current_keyword_string != new_human_value:
                     differences.append({'pk':gloss.pk,
@@ -86,23 +86,23 @@ def compare_valuedict_to_gloss(valuedict,gloss):
 
             #If not, find the matching field in the gloss, and remember its 'real' name
             try:
-                field = fields[human_key];
-                machine_key = field.name;
+                field = fields[human_key]
+                machine_key = field.name
             except KeyError:
-                continue;
+                continue
 
             #Try to translate the value to machine values if needed
             if len(field.choices) > 0:
-                human_to_machine_values = {human_value: machine_value for machine_value, human_value in field.choices};
+                human_to_machine_values = {human_value: machine_value for machine_value, human_value in field.choices}
 
                 try:
-                    new_machine_value = human_to_machine_values[new_human_value];
+                    new_machine_value = human_to_machine_values[new_human_value]
                 except KeyError:
 
                     #If you can't find a corresponding human value, maybe it's empty
                     if new_human_value in ['',' ']:
-                        new_human_value = 'None';
-                        new_machine_value = None;
+                        new_human_value = 'None'
+                        new_machine_value = None
 
                     #If not, stop trying
                     else:
@@ -112,29 +112,29 @@ def compare_valuedict_to_gloss(valuedict,gloss):
             elif field.__class__.__name__ == 'IntegerField':
 
                 try:
-                    new_machine_value = int(new_human_value);
+                    new_machine_value = int(new_human_value)
                 except ValueError:
-                    new_human_value = 'None';
-                    new_machine_value = None;
+                    new_human_value = 'None'
+                    new_machine_value = None
             elif field.__class__.__name__ == 'NullBooleanField':
 
                 if new_human_value in ['True','true']:
-                    new_machine_value = True;
+                    new_machine_value = True
                 else:
-                    new_machine_value = False;
+                    new_machine_value = False
 
             #If all the above does not apply, this is a None value or plain text
             else:
                 if new_human_value == 'None':
-                    new_machine_value = None;
+                    new_machine_value = None
                 else:
                     new_machine_value = new_human_value
 
             #Try to translate the key to machine keys if possible
             try:
-                original_machine_value = getattr(gloss,machine_key);
+                original_machine_value = getattr(gloss,machine_key)
             except KeyError:
-                continue;
+                continue
 
             #Translate back the machine value from the gloss
             try:
@@ -147,7 +147,7 @@ def compare_valuedict_to_gloss(valuedict,gloss):
                 new_machine_value = unescape(new_machine_value)
                 new_human_value = unescape(new_human_value)
             except TypeError:
-                pass;
+                pass
 
             #Check for change, and save your findings if there is one
             if original_machine_value != new_machine_value:
@@ -166,7 +166,7 @@ def reload_signbank(request=None):
     """Functions to clear the cache of Apache, also works as view"""
 
     #Refresh the wsgi script
-    os.utime(WSGI_FILE,None);
+    os.utime(WSGI_FILE,None)
 
     #If this is an HTTP request, give an HTTP response
     if request != None:
