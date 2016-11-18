ffdf1e9e3fda62a625503294b6844710962bc652
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-11-18 13:32:11 +0100

    Fixed searching videos for their glosses #203

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -4,6 +4,6 @@ from models import Video, GlossVideo
 #admin.site.register(Video)
 
 class GlossVideoAdmin(admin.ModelAdmin):
-    search_fields = ['^gloss']
+    search_fields = ['^gloss__idgloss']
     
 admin.site.register(GlossVideo, GlossVideoAdmin)
