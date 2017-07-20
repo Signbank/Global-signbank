1b8093ebba805514a5b738ce0da2248290574d7e
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2017-07-20 16:54:35 +0200

    Gloss could not be changed in the admin. Fixed. #224.

--- signbank/dictionary/admin.py
+++ signbank/dictionary/admin.py
@@ -82,7 +82,7 @@ class GlossAdmin(VersionAdmin):
     if SEPARATE_ENGLISH_IDGLOSS_FIELD:
         idgloss_fields.append('annotation_idgloss_en')
 
-    fieldsets = ((None, {'fields': tuple(idgloss_fields)+tuple(FIELDS['main'])+('language', 'dialect')}, ),
+    fieldsets = ((None, {'fields': tuple(idgloss_fields)+tuple(FIELDS['main'])+('signlanguage', 'dialect')}, ),
                  ('Publication Status', {'fields': ('inWeb',  'isNew', 'creator','creationDate','alternative_id'),
                                        'classes': ('collapse',)}, ),
                  ('Phonology', {'fields': FIELDS['phonology'], 'classes': ('collapse',)}, ),
