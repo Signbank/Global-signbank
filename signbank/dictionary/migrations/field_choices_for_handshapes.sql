e001f7a040fde3f3782fb681827fae2b3639a51f
susan <mijnoog@kpnmail.nl>
2017-08-10 11:49:31 +0200

    Add Field Choices to support Handshapes

---
+++ signbank/dictionary/migrations/field_choices_for_handshapes.sql
@@ -0,0 +1,63 @@
+
+INSERT INTO dictionary_fieldchoice VALUES(1121,'Spreading','Unspread','Unspread',3,'Unspread');"
+INSERT INTO dictionary_fieldchoice VALUES(1122,'Spreading','Spread','Spread',4,'Spread');"
+INSERT INTO dictionary_fieldchoice VALUES(1123,'Spreading','Stacked','Stacked',5,'Stacked');"
+INSERT INTO dictionary_fieldchoice VALUES(1124,'Spreading','Crossed','Crossed',6,'Crossed');"
+
+INSERT INTO dictionary_fieldchoice\" VALUES(1140,'Thumb','Extended','Extended',3,'Extended');"
+INSERT INTO dictionary_fieldchoice\" VALUES(1141,'Thumb','Opposed','Opposed',4,'Opposed');"
+
+INSERT INTO dictionary_fieldchoice VALUES(1151,'Aperture','Open','Open',3,'Open');"
+INSERT INTO dictionary_fieldchoice VALUES(1152,'Aperture','Closed','Closed',4,'Closed');"
+
+INSERT INTO dictionary_fieldchoice VALUES(1100,'JointConfiguration','extended','extended',3,'extended');"
+INSERT INTO dictionary_fieldchoice VALUES(1101,'JointConfiguration','open','open',4,'open');"
+INSERT INTO dictionary_fieldchoice VALUES(1102,'JointConfiguration','curved','curved',5,'curved');"
+INSERT INTO dictionary_fieldchoice VALUES(1103,'JointConfiguration','curved-open','curved-open',6,'curved-open');"
+INSERT INTO dictionary_fieldchoice VALUES(1104,'JointConfiguration','curved-closed','curved-closed',7,'curved-closed');"
+INSERT INTO dictionary_fieldchoice VALUES(1105,'JointConfiguration','bent','bent',8,'bent');"
+INSERT INTO dictionary_fieldchoice VALUES(1106,'JointConfiguration','flat','flat',9,'flat');"
+INSERT INTO dictionary_fieldchoice VALUES(1107,'JointConfiguration','flat-open','flat-open',10,'flat-open');"
+INSERT INTO dictionary_fieldchoice VALUES(1108,'JointConfiguration','flat-closed','flat-closed',11,'flat-closed');"
+INSERT INTO dictionary_fieldchoice VALUES(1109,'JointConfiguration','closed','closed',12,'closed');"
+INSERT INTO dictionary_fieldchoice VALUES(1110,'JointConfiguration','crossed','crossed',13,'crossed');"
+
+INSERT INTO dictionary_fieldchoice VALUES(1130,'Quantity','one','one',3,'one');
+INSERT INTO dictionary_fieldchoice VALUES(1131,'Quantity','two','two',4,'two');
+INSERT INTO dictionary_fieldchoice VALUES(1132,'Quantity','three','three',5,'three');
+INSERT INTO dictionary_fieldchoice VALUES(1133,'Quantity','four','four',6,'four');
+INSERT INTO dictionary_fieldchoice VALUES(1134,'Quantity','all','all',7,'all');
+
+INSERT INTO dictionary_fieldchoice VALUES(1161,'FingerSelection','TIMRP','TIMRP',3,'TIMRP');
+INSERT INTO dictionary_fieldchoice VALUES(1162,'FingerSelection','TIMR','TIMR',4,'TIMR');
+INSERT INTO dictionary_fieldchoice VALUES(1163,'FingerSelection','TIMP','TIMP',5,'TIMP');
+INSERT INTO dictionary_fieldchoice VALUES(1164,'FingerSelection','TIRP','TIRP',6,'TIRP');
+INSERT INTO dictionary_fieldchoice VALUES(1165,'FingerSelection','TMRP','TMRP',7,'TMRP');
+INSERT INTO dictionary_fieldchoice VALUES(1166,'FingerSelection','TIM','TIM',8,'TIM');
+INSERT INTO dictionary_fieldchoice VALUES(1167,'FingerSelection','TIR','TIR',9,'TIR');
+INSERT INTO dictionary_fieldchoice VALUES(1168,'FingerSelection','TIP','TIP',10,'TIP');
+INSERT INTO dictionary_fieldchoice VALUES(1169,'FingerSelection','TMR','TMR',11,'TMR');
+INSERT INTO dictionary_fieldchoice VALUES(1170,'FingerSelection','TMP','TMP',12,'TMP');
+INSERT INTO dictionary_fieldchoice VALUES(1171,'FingerSelection','TRP','TRP',13,'TRP');
+INSERT INTO dictionary_fieldchoice VALUES(1172,'FingerSelection','TI','TI',14,'TI');
+INSERT INTO dictionary_fieldchoice VALUES(1173,'FingerSelection','TM','TM',15,'TM');
+INSERT INTO dictionary_fieldchoice VALUES(1174,'FingerSelection','TR','TR',16,'TR');
+INSERT INTO dictionary_fieldchoice VALUES(1175,'FingerSelection','TP','TP',17,'TP');
+INSERT INTO dictionary_fieldchoice VALUES(1176,'FingerSelection','T','T',18,'T');
+INSERT INTO dictionary_fieldchoice VALUES(1177,'FingerSelection','IMRP','IMRP',19,'IMRP');
+INSERT INTO dictionary_fieldchoice VALUES(1178,'FingerSelection','IMR','IMR',20,'IMR');
+INSERT INTO dictionary_fieldchoice VALUES(1179,'FingerSelection','IMP','IMP',21,'IMP');
+INSERT INTO dictionary_fieldchoice VALUES(1180,'FingerSelection','IRP','IRP',22,'IRP');
+INSERT INTO dictionary_fieldchoice VALUES(1181,'FingerSelection','IR','IR',23,'IR');
+INSERT INTO dictionary_fieldchoice VALUES(1182,'FingerSelection','IM','IM',24,'IM');
+INSERT INTO dictionary_fieldchoice VALUES(1183,'FingerSelection','IP','IP',25,'IP');
+INSERT INTO dictionary_fieldchoice VALUES(1184,'FingerSelection','I','I',26,'I');
+INSERT INTO dictionary_fieldchoice VALUES(1185,'FingerSelection','MRP','MRP',27,'MRP');
+INSERT INTO dictionary_fieldchoice VALUES(1186,'FingerSelection','MR','MR',28,'MR');
+INSERT INTO dictionary_fieldchoice VALUES(1187,'FingerSelection','MP','MP',29,'MP');
+INSERT INTO dictionary_fieldchoice VALUES(1188,'FingerSelection','M','M',30,'M');
+INSERT INTO dictionary_fieldchoice VALUES(1189,'FingerSelection','RP','RP',31,'RP');
+INSERT INTO dictionary_fieldchoice VALUES(1190,'FingerSelection','R','R',32,'R');
+INSERT INTO dictionary_fieldchoice VALUES(1191,'FingerSelection','P','P',33,'P');
+INSERT INTO dictionary_fieldchoice VALUES(1192,'FingerSelection','','',34,'');
+
