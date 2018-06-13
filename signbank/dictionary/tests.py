dc537a840e1e35eecd883807dc30e178a1b1e904
Wessel Stoop <stoopwessel@gmail.com>
2018-06-13 09:03:09 -0400

    Removed weird post argument from create gloss unit test, will investigate later what it was for

--- signbank/dictionary/tests.py
+++ signbank/dictionary/tests.py
@@ -93,12 +93,13 @@ class BasicCRUDTests(TestCase):
         test_dataset = Dataset.objects.get(name=dataset_name)
 
         # Construct the Create Gloss form data
-        create_gloss_form_data = {'dataset': test_dataset.id, 'idgloss': "idgloss_test", 'glosscreate_id': ''}
+        create_gloss_form_data = {'dataset': test_dataset.id, 'idgloss': "idgloss_test"}
         for language in test_dataset.translation_languages.all():
             create_gloss_form_data[GlossCreateForm.gloss_create_field_prefix + language.language_code_2char] = \
                 "annotationidglosstranslation_test_" + language.language_code_2char
 
         # User does not have permission to change dataset. Creating a gloss should fail.
+        print('CREATE FORM DATA',create_gloss_form_data)
         response = client.post('/dictionary/update/gloss/', create_gloss_form_data)
 
         self.assertEqual(response.status_code, 200)
