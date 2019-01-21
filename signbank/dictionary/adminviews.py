4658f616f84138fd8e706e79dc43e58fadb6a63f
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2019-01-21 12:18:20 +0100

    Split the Lemma column in the search table to multiple columns (one per language).

--- signbank/dictionary/adminviews.py
+++ signbank/dictionary/adminviews.py
@@ -85,6 +85,16 @@ def order_queryset_by_sort_order(get, qs):
         qs = qs.annotate(**{sOrderAsc: Subquery(annotationidglosstranslation.values('text')[:1])}).order_by(sOrder)
         return qs
 
+    def order_queryset_by_lemmaidglosstranslation(qs, sOrder):
+        language_code_2char = sOrder[-2:]
+        sOrderAsc = sOrder
+        if (sOrder[0:1] == '-'):
+            # A starting '-' sign means: descending order
+            sOrderAsc = sOrder[1:]
+        lemmaidglosstranslation = LemmaIdglossTranslation.objects.filter(lemma=OuterRef('lemma'), language__language_code_2char__iexact=language_code_2char)
+        qs = qs.annotate(**{sOrderAsc: Subquery(lemmaidglosstranslation.values('text')[:1])}).order_by(sOrder)
+        return qs
+
     # Set the default sort order
     sOrder = 'lemma__lemmaidglosstranslation__text'  # Default sort order if nothing is specified
     # See if the form contains any sort-order information
@@ -103,6 +113,8 @@ def order_queryset_by_sort_order(get, qs):
         ordered = order_queryset_by_tuple_list(qs, sOrder, "Location")
     elif "annotationidglosstranslation_order_" in sOrder:
         ordered = order_queryset_by_annotationidglosstranslation(qs, sOrder)
+    elif "lemmaidglosstranslation_order_" in sOrder:
+        ordered = order_queryset_by_lemmaidglosstranslation(qs, sOrder)
     else:
         # Use straightforward ordering on field [sOrder]
 
