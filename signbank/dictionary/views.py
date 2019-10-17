84d86e3e5833db77a6f97528e3383f1d17a9aab1
susanodd <mijnoog@kpnmail.nl>
2019-10-17 16:12:46 +0200

    Issue #354: Frequency View tab for Glosses. Existing Region Frequencies displayed.

--- signbank/dictionary/views.py
+++ signbank/dictionary/views.py
@@ -2605,3 +2605,23 @@ def gloss_revision_history(request,gloss_pk):
                    'SHOW_DATASET_INTERFACE_OPTIONS': show_dataset_interface
                    })
 
+def gloss_frequency(request,gloss_pk):
+
+    gloss = Gloss.objects.get(pk=gloss_pk)
+
+    selected_datasets = get_selected_datasets_for_user(request.user)
+    dataset_languages = Language.objects.filter(dataset__in=selected_datasets).distinct()
+
+    if hasattr(settings, 'SHOW_DATASET_INTERFACE_OPTIONS') and settings.SHOW_DATASET_INTERFACE_OPTIONS:
+        show_dataset_interface = settings.SHOW_DATASET_INTERFACE_OPTIONS
+    else:
+        show_dataset_interface = False
+
+    return render(request, 'dictionary/gloss_frequency.html',
+                  {'gloss': gloss,
+                   'frequency_regions': settings.FREQUENCY_REGIONS,
+                   'data_datasets': gloss.data_datasets(),
+                   'dataset_languages': dataset_languages,
+                   'selected_datasets': selected_datasets,
+                   'SHOW_DATASET_INTERFACE_OPTIONS': show_dataset_interface
+                   })
