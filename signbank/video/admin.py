ae61984a24f1cc0801d4621c81b882154ce99098
Micha Hulsbosch <michahulsbosch@gmail.com>
2020-03-30 12:19:33 +0200

    Search for GlossVideo in Admin was broken. Fixed. #635

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -4,6 +4,6 @@ from signbank.video.models import Video, GlossVideo
 #admin.site.register(Video)
 
 class GlossVideoAdmin(admin.ModelAdmin):
-    search_fields = ['^gloss__idgloss']
+    search_fields = ['^gloss__annotationidglosstranslation__text']
     
 admin.site.register(GlossVideo, GlossVideoAdmin)
