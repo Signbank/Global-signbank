c01a7e6cb0910a252c55b4f1818ea0bd119db2ac
Micha Hulsbosch <michahulsbosch@gmail.com>
2021-03-18 12:20:23 +0100

    Remove hardcoded language stuff for Handshape list view typeahead

--- media/js/handshape_search.js
+++ media/js/handshape_search.js
@@ -36,11 +36,11 @@ function handshapetypeahead(target) {
 
      $(target).typeahead(null, {
           name: 'handshapetarget',
-          displayKey: current_language_code+'_name',
+          displayKey: 'name',
           source: handshape_bloodhound.ttAdapter(),
           templates: {
               suggestion: function(hs) {
-                  return("<p><strong>" + hs[current_language_code + '_name'] + "</strong></p>");
+                  return("<p><strong>" + hs['name'] + "</strong></p>");
               }
           }
       });
