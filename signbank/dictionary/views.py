69a44cda6e900a3fc62d2f453a3bf38ffae40d43
susanodd <mijnoog@kpnmail.nl>
2023-01-12 16:15:07 +0100

    Issue #658, #858: Removed obsolete parameter thanks to @Jetske

--- signbank/dictionary/views.py
+++ signbank/dictionary/views.py
@@ -306,8 +306,7 @@ def gloss(request, glossid):
     notes = gloss.definition_set.all()
     notes_groupedby_role = {}
     for note in notes:
-        translated_note_role = machine_value_to_translated_human_value(note.role, note_role_choices,
-                                                                       request.LANGUAGE_CODE)
+        translated_note_role = machine_value_to_translated_human_value(note.role, note_role_choices)
         role_id = (note.role, translated_note_role)
         if role_id not in notes_groupedby_role:
             notes_groupedby_role[role_id] = []
