e9653d23a0f3ea96609c9e20dad75250f4393dac
susanodd <mijnoog@kpnmail.nl>
2023-03-06 10:28:39 +0100

    #886: Added filters to GlossVideoAdmin, file exists, file system group.
    Added setting for file system groups.

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django import forms
 from django.db import models
 from signbank.video.models import GlossVideo, Dataset
-from signbank.settings.server_specific import WRITABLE_FOLDER
+from signbank.settings.base import *
+from signbank.settings.server_specific import WRITABLE_FOLDER, FILESYSTEM_SIGNBANK_GROUPS
 from django.utils.translation import override, ugettext_lazy as _
 from django.core.files.storage import FileSystemStorage
 from django.core.files import File
+from django.db.models import Q, F, Case, Sum, Count, IntegerField, Value, When, ExpressionWrapper
+
 
 
 class GlossVideoAdminForm(forms.ModelForm):
@@ -36,10 +39,72 @@ class GlossVideoDatasetFilter(admin.SimpleListFilter):
         return queryset.all()
 
 
+class GlossVideoFileSystemGroupFilter(admin.SimpleListFilter):
+
+    title = _('File System Group')
+    parameter_name = 'filesystem_group'
+
+    def lookups(self, request, model_admin):
+        filesystem_groups = tuple((g, g) for g in FILESYSTEM_SIGNBANK_GROUPS)
+        return filesystem_groups
+
+    def queryset(self, request, queryset):
+
+        def matching_file_group(videofile, key):
+            if not key:
+                return False
+            from pathlib import Path
+            video_file_full_path = Path(WRITABLE_FOLDER, videofile)
+            if video_file_full_path.exists():
+                return video_file_full_path.group() == key
+            else:
+                return False
+
+        queryset_res = queryset.values('id', 'videofile')
+        results = [qv['id'] for qv in queryset_res
+                   if matching_file_group(qv['videofile'], self.value())]
+
+        if self.value():
+            return queryset.filter(id__in=results)
+        else:
+            return queryset.all()
+
+
+class GlossVideoExistenceFilter(admin.SimpleListFilter):
+
+    title = _('File Exists')
+    parameter_name = 'file_exists'
+
+    def lookups(self, request, model_admin):
+        file_exists = tuple((b, b) for b in ('True', 'False'))
+        return file_exists
+
+    def queryset(self, request, queryset):
+
+        def matching_file_exists(videofile, key):
+            if not key:
+                return False
+            from pathlib import Path
+            video_file_full_path = Path(WRITABLE_FOLDER, videofile)
+            if video_file_full_path.exists():
+                return key == 'True'
+            else:
+                return key == 'False'
+
+        queryset_res = queryset.values('id', 'videofile')
+        results = [qv['id'] for qv in queryset_res
+                   if matching_file_exists(qv['videofile'], self.value())]
+
+        if self.value():
+            return queryset.filter(id__in=results)
+        else:
+            return queryset.all()
+
+
 class GlossVideoAdmin(admin.ModelAdmin):
 
-    list_display = ['id', 'gloss', 'video_file', 'version']
-    list_filter = (GlossVideoDatasetFilter,)
+    list_display = ['id', 'gloss', 'video_file', 'file_exists', 'file_group', 'version']
+    list_filter = (GlossVideoDatasetFilter, GlossVideoFileSystemGroupFilter, GlossVideoExistenceFilter)
 
     search_fields = ['^gloss__annotationidglosstranslation__text']
 
@@ -54,6 +119,29 @@ class GlossVideoAdmin(admin.ModelAdmin):
         video_file_full_path = os.path.join(WRITABLE_FOLDER, str(obj.videofile))
 
         return video_file_full_path
+
+    def file_exists(self, obj=None):
+        # this will display a Boolean in the list view
+        if obj is None:
+            return False
+        import os
+        video_file_full_path = os.path.join(WRITABLE_FOLDER, str(obj.videofile))
+        if os.path.exists(video_file_full_path):
+            return True
+        else:
+            return False
+
+    def file_group(self, obj=None):
+        # this will display a group in the list view
+        if obj is None:
+            return ""
+        else:
+            from pathlib import Path
+            video_file_full_path = Path(WRITABLE_FOLDER, str(obj.videofile))
+            if video_file_full_path.exists():
+                return video_file_full_path.group()
+            else:
+                return ""
 
     def get_list_display_links(self, request, list_display):
         # do not allow the user to view individual revisions in list
