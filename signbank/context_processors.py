82c0d07eb808f72309b4fac92c3a51c13109b65a
susanodd <s.johanna.even@hetnet.nl>
2024-06-19 07:14:34 +0200

    #1259: Revised signbank header to wrap properly
    Revised NME description editing OK to not excede layout. Revised NME to put videos on next row if not enough space.

--- signbank/context_processors.py
+++ signbank/context_processors.py
@@ -1,13 +1,14 @@
 from django.conf import settings
-from guardian.shortcuts import get_objects_for_user
+from guardian.shortcuts import get_objects_for_user, get_user_perms
 from signbank.tools import get_selected_datasets_for_user, get_datasets_with_public_glosses
 from signbank.dictionary.models import Dataset
+
 
 def url(request):
 
     if not request.user.is_authenticated:
         # for anonymous users, show datasets with public glosses in header
-        viewable_datasets = get_datasets_with_public_glosses()
+        viewable_datasets = list(get_datasets_with_public_glosses())
 
         if 'selected_datasets' in request.session.keys():
             selected_datasets = Dataset.objects.filter(acronym__in=request.session['selected_datasets'])
@@ -16,7 +17,13 @@ def url(request):
             selected_datasets = Dataset.objects.filter(acronym=settings.DEFAULT_DATASET_ACRONYM)
     else:
         # display all datasets in header
-        viewable_datasets = Dataset.objects.all()
+        viewable_datasets = list(get_datasets_with_public_glosses())
+        for dataset in Dataset.objects.all():
+            if dataset in viewable_datasets:
+                continue
+            permissions_for_dataset = get_user_perms(request.user, dataset)
+            if 'view_dataset' in permissions_for_dataset or 'can_view_dataset' in permissions_for_dataset:
+                viewable_datasets.append(dataset)
         selected_datasets = get_selected_datasets_for_user(request.user)
 
     return {'URL': settings.URL,
@@ -26,6 +33,6 @@ def url(request):
             'SHOW_DATASET_INTERFACE_OPTIONS': settings.SHOW_DATASET_INTERFACE_OPTIONS,
             'SEPARATE_ENGLISH_IDGLOSS_FIELD':settings.SEPARATE_ENGLISH_IDGLOSS_FIELD,
             'CROP_GLOSS_IMAGES': settings.CROP_GLOSS_IMAGES,
-			'INTERFACE_LANGUAGE_CODES': [language_code for language_code, full_name in settings.LANGUAGES],
-			'INTERFACE_LANGUAGE_SHORT_NAMES': settings.INTERFACE_LANGUAGE_SHORT_NAMES
-			}
+            'INTERFACE_LANGUAGE_CODES': [language_code for language_code, full_name in settings.LANGUAGES],
+            'INTERFACE_LANGUAGE_SHORT_NAMES': settings.INTERFACE_LANGUAGE_SHORT_NAMES
+            }
