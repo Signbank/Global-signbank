c462bb75b64ed555a492f6710baf710d5cc4c226
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-06-27 11:08:18 +0200

    fix: Add a decorator to put API user in request #1278

--- signbank/api_token.py
+++ signbank/api_token.py
@@ -3,6 +3,10 @@ import hashlib
 import secrets
 import string
 
+from django.http import HttpRequest
+
+from signbank.dictionary.models import SignbankAPIToken
+
 
 def generate_auth_token(length=16):
     """Generate a random authentication token."""
@@ -16,3 +20,27 @@ def hash_token(token):
     return hash_object.hexdigest()
 
 
+def get_api_user(request):
+    """Return a user based if there is an API token in the request, else None"""
+    auth_token_request = request.headers.get('Authorization', '')
+    if not auth_token_request:
+        return None
+
+    auth_token = auth_token_request.removeprefix('Bearer ')
+    if not auth_token:
+        return None
+
+    hashed_token = hash_token(auth_token)
+    signbank_token = SignbankAPIToken.objects.filter(api_token=hashed_token).first()
+    return signbank_token.signbank_user if signbank_token else None
+
+
+def put_api_user_in_request(func):
+    """A decorator to replace the request.user with the user found by checking an API token"""
+    def wrapper(*args, **kwargs):
+        if isinstance(args[0], HttpRequest):
+            request = args[0]
+            if api_user := get_api_user(request):
+                request.user = api_user
+        return func(*args, **kwargs)
+    return wrapper
