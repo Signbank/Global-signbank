00f4039fda607d0b7e9cc34bba608c40b5fae64b
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-07-01 09:50:24 +0200

    feat: Add error handling to API token check #1279

--- signbank/api_token.py
+++ signbank/api_token.py
@@ -3,7 +3,8 @@ import hashlib
 import secrets
 import string
 
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
+from django.utils.translation import gettext_lazy as _
 
 from signbank.dictionary.models import SignbankAPIToken
 
@@ -20,27 +21,50 @@ def hash_token(token):
     return hash_object.hexdigest()
 
 
+class APIAuthException(Exception):
+    """ Exception class to raise any problems in authorizing a user for the API"""
+    pass
+
+
 def get_api_user(request):
-    """Return a user based if there is an API token in the request, else None"""
+    """
+    Return a user if there is a correct API token in the request.
+    The HTTP header must contain:
+
+    Authorization:"Bearer XXXXXX"
+
+    where XXXXXX represents the user's API Token
+    """
     auth_token_request = request.headers.get('Authorization', '')
     if not auth_token_request:
         return None
 
-    auth_token = auth_token_request.removeprefix('Bearer ')
+    auth_token = auth_token_request.removeprefix('Bearer').strip()
     if not auth_token:
-        return None
+        raise APIAuthException(_("No Authorization token found"))
 
     hashed_token = hash_token(auth_token)
     signbank_token = SignbankAPIToken.objects.filter(api_token=hashed_token).first()
-    return signbank_token.signbank_user if signbank_token else None
+    if not signbank_token:
+        raise APIAuthException(_("Your Authorization Token does not match anything."))
+
+    return signbank_token.signbank_user
 
 
 def put_api_user_in_request(func):
     """A decorator to replace the request.user with the user found by checking an API token"""
     def wrapper(*args, **kwargs):
-        if isinstance(args[0], HttpRequest):
-            request = args[0]
-            if api_user := get_api_user(request):
-                request.user = api_user
+        if not args or not isinstance(args[0], HttpRequest):
+            return func(*args, **kwargs)
+
+        request = args[0]
+
+        try:
+            api_user = get_api_user(request)
+        except APIAuthException as api_auth_exception:
+            return JsonResponse({'errors': str(api_auth_exception)})
+
+        if api_user:
+            request.user = api_user
         return func(*args, **kwargs)
     return wrapper
