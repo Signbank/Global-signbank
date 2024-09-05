154b639617e39b4ed61c7180b0f38eaf0f5a7193
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-09-05 09:47:36 +0200

    refactor: Move fixture files to designated folder #1270

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,10 +4,11 @@ bin/develop.py createsuperuser
 echo 'from django.contrib.contenttypes.models import ContentType; ContentType.objects.all().delete()' | bin/develop.py shell
 echo 'from signbank.dictionary.models import FieldChoice; FieldChoice.objects.all().delete()' | bin/develop.py shell
 
-bin/develop.py loaddata content_types.json
-bin/develop.py loaddata permissions.json
-bin/develop.py loaddata groups.json
-bin/develop.py loaddata fieldchoices.json
-bin/develop.py loaddata pages.json
+bin/develop.py loaddata fixtures/content_types.json
+bin/develop.py loaddata fixtures/permissions.json
+bin/develop.py loaddata fixtures/groups.json
+bin/develop.py loaddata fixtures/fieldchoices.json
+bin/develop.py loaddata fixtures/pages.json
 
-# Add yourself to Dataset_Manager group in admin
+echo
+echo "IMPORTANT: Add yourself to Dataset_Manager group in admin"
