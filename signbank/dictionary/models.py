e3ebbbaec317ab827dba974bcb21066ffb346e1e
susanodd <mijnoog@kpnmail.nl>
2024-10-24 13:18:12 +0200

    #1344: Circular import in models.

--- signbank/dictionary/models.py
+++ signbank/dictionary/models.py
@@ -2314,6 +2314,7 @@ class Gloss(models.Model):
         glossvideo = GlossVideo.objects.filter(gloss=self, glossvideonme=None, glossvideoperspective=None).filter(version=0).first()
         if not glossvideo:
             # calculate image path independent of video
+            from signbank.tools import get_default_annotationidglosstranslation
             annotation = get_default_annotationidglosstranslation(self)
             idgloss = self.idgloss
             two_letter_dir = idgloss[:2]
