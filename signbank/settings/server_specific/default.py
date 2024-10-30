a7f4b95f3f34fc48da58a471035ffa3b529aadbe
Jetske <to_jetske@hotmail.com>
2024-10-30 14:23:31 +0100

    Only add senses to ecv descriptions, not phonology #1325

--- signbank/settings/server_specific/default.py
+++ signbank/settings/server_specific/default.py
@@ -204,9 +204,9 @@ ECV_FILE = 'myecv.ecv'
 
 #What do you want to include in the ECV file?
 ECV_SETTINGS = {
-    'include_phonology_and_frequencies': True,
+    'include_phonology_and_frequencies': False,
 
-    'description_fields' : ['handedness', 'tokNo'],
+    'description_fields' : [],
 
     # The order of languages matters as the first will
     # be treated as default by ELAN
