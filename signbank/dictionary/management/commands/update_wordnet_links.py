c335fd701724f9d5eeacd5815922a22be2829523
Jetske <to_jetske@hotmail.com>
2024-10-31 16:26:44 +0100

    Read the downloaded files and save in dictionary #1350

--- signbank/dictionary/management/commands/update_wordnet_links.py
+++ signbank/dictionary/management/commands/update_wordnet_links.py
@@ -38,16 +38,62 @@ class Command(BaseCommand):
             "submit": "links" 
         }
         download_response = session.post(DOWNLOAD_URL, data=download_payload)
-
-        # Check if the download was successful and save the file
         if download_response.status_code == 200:
-            filepath = os.path.join(WRITABLE_FOLDER, "wordnet_links.csv")
-            with open(filepath, "wb") as f:
+            with open(os.path.join(WRITABLE_FOLDER, "wordnet_links.csv"), "wb") as f:
                 f.write(download_response.content)
-            print("CSV downloaded successfully!")
+            print("Links CSV downloaded successfully!")
         else:
-            print("Failed to download CSV. Status code:", download_response.status_code)
+            print("Failed to download Links CSV. Status code:", download_response.status_code)
+
+        # Download the links CSV file
+        download_payload = {
+            "submit": "signs" 
+        }
+        download_response = session.post(DOWNLOAD_URL, data=download_payload)
+        if download_response.status_code == 200:
+            with open(os.path.join(WRITABLE_FOLDER, "wordnet_signs.csv"), "wb") as f:
+                f.write(download_response.content)
+            print("Signs CSV downloaded successfully!")
+        else:
+            print("Failed to download Signs CSV. Status code:", download_response.status_code)
+
+
+    def get_links_data(self):
+        import csv
+
+        signs = {}
+        with open(os.path.join(WRITABLE_FOLDER, "wordnet_signs.csv"), mode='r') as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                wordnet_sign_id = row[0]
+                signbank_sign_id = row[1]
+                wordnet_sign_id = wordnet_sign_id.replace("ngt.", "").replace("'", '').replace(" ", "")
+                signbank_sign_id = signbank_sign_id.replace("'", '').replace(" ", "")
+                signs[wordnet_sign_id] = signbank_sign_id
+
+        links = {}
+        link_base = "https://www.sign-lang.uni-hamburg.de/easier/sign-wordnet/synset/"
+        with open(os.path.join(WRITABLE_FOLDER, "wordnet_links.csv"), mode='r') as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                for r_i, r in enumerate(row):
+                    row[r_i] = r.replace("'", '').replace(" ", "")
+                wordnet_sign_id = row[0].replace("ngt.", "")
+
+                link = f"{link_base}{row[1]}.html"
+                links_list = [row[1], row[2], row[3], link]
+                sign_id = wordnet_sign_id
+                if wordnet_sign_id in signs:
+                    sign_id = signs[wordnet_sign_id]
+                
+                # add info to links
+                if signs[wordnet_sign_id] not in links:
+                    links[sign_id]=[]
+                links[sign_id].append(links_list)
+        
+        return links
+
 
     def handle(self, *args, **options):
-        
         self.download_links_csv()
+        links = self.get_links_data()
