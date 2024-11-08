13ec7c59440e93df3f3c01c940a3a79f94f4afc0
Jetske <to_jetske@hotmail.com>
2024-11-08 16:12:36 +0100

    Better documentation #1350

--- signbank/dictionary/management/commands/update_wordnet_links.py
+++ signbank/dictionary/management/commands/update_wordnet_links.py
@@ -1,18 +1,26 @@
-"""This script updates the WordNet links in the database."""
+"""This script updates the WordNet links in the database. This works ONLY for NGT dataset."""
 
 import os
 import shutil
 import requests
+import csv
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import BaseCommand
+
+from signbank.dictionary.models import Synset, Dataset, Gloss
 from signbank.settings.server_specific import WORDNET_USERNAME, WORDNET_PASSWORD, WRITABLE_FOLDER
 
+import nltk
+from nltk.corpus import wordnet as wn
+from bs4 import BeautifulSoup
+
 class Command(BaseCommand):
-    help = 'Update the WordNet links in the database.'
+    help = 'Update the WordNet links in the database. This works ONLY for NGT dataset.'
 
     def download_links_csv(self):
+        """ Download the WordNet links CSV file from the server. """
 
-        # Replace these with the actual URLs and credentials
         LOGIN_URL = "https://signwordnetannotation.pythonanywhere.com/login.html"
         DOWNLOAD_URL = "https://signwordnetannotation.pythonanywhere.com/generate_csv.html" 
         EXPECTED_LANDING_URL = "https://signwordnetannotation.pythonanywhere.com/"
@@ -59,8 +67,9 @@ class Command(BaseCommand):
 
 
     def get_links_data(self):
-        import csv
+        """ Read the WordNet links CSV file and return the data. """
 
+        # Read the signs CSV file
         signs = {}
         with open(os.path.join(WRITABLE_FOLDER, "synsets/wordnet_signs.csv"), mode='r') as file:
             csv_reader = csv.reader(file)
@@ -71,6 +80,7 @@ class Command(BaseCommand):
                 signbank_sign_id = signbank_sign_id.replace("'", '').replace(" ", "")
                 signs[wordnet_sign_id] = signbank_sign_id
 
+        # Read the links CSV file
         links = {}
         link_base = "https://www.sign-lang.uni-hamburg.de/easier/sign-wordnet/synset/"
         with open(os.path.join(WRITABLE_FOLDER, "synsets/wordnet_links.csv"), mode='r') as file:
@@ -94,11 +104,12 @@ class Command(BaseCommand):
     
 
     def get_lemma_definitions(self):
-        from nltk.corpus import wordnet as wn
+        """ Get the lemma names and definitions from the WordNet gloss file. """
 
         with open(os.path.join(WRITABLE_FOLDER, 'synsets/sign_wordnet_gloss_dse.tab'), 'r') as file:
             rows = file.readlines()
             
+        # Save the lemma names and definitions in a dictionary
         wn_dict = {}
         for row in rows[1:]:
             row_split = row.split('\t')
@@ -111,8 +122,11 @@ class Command(BaseCommand):
         return wn_dict
 
     def find_lemmas_description_in_html(self, html):
-        from bs4 import BeautifulSoup
+        """ Find the lemmas and description in the HTML of the Multilingual Sign Language Wordnet page. """
+        
         soup = BeautifulSoup(html, "html.parser")
+
+        # Scrape the lemmas from the HTML
         lemmas_paragraph = None
         for p in soup.find_all("p"):
             if "Lemmas:" in p.get_text():
@@ -123,17 +137,17 @@ class Command(BaseCommand):
         if lemmas_paragraph is None:
             lemmas = ""
 
+        # Scrape the definition from the HTML
         definition_paragraph = soup.find("p", class_="synset_def")
         if definition_paragraph:
             definition = definition_paragraph.get_text().replace("Definition:", "").strip()
         else:
             definition = ""
+
         return lemmas, definition
     
     def update_links_data(self, links, wn_dict):
-        from signbank.dictionary.models import Synset, Dataset, Gloss
-        from django.core.exceptions import ObjectDoesNotExist
-        import requests
+        """ Update the WordNet links in the database. """
 
         ngt_dataset = Dataset.objects.get(acronym="NGT")
         Synset.objects.filter(dataset = ngt_dataset).delete()
@@ -146,20 +160,24 @@ class Command(BaseCommand):
             gloss = Gloss.objects.filter(id=int(gloss_id)).first()
             if not gloss:
                 continue 
-
+            
+            # Create the Synset objects and add them to the Gloss
             for l in links[gloss_id]:
                 synset = Synset.objects.filter(name = l[0], dataset = ngt_dataset).first()
                 if not synset:
                     synset = Synset.objects.create(name = l[0], dataset = ngt_dataset)
 
+                    # Check if lemma and description are available in the WordNet gloss file
                     if l[0] in wn_dict:
                         lemmas_string = ', '.join(wn_dict[l[0]][0])
                         synset.lemmas = lemmas_string
                         synset.description = wn_dict[l[0]][1]
 
+                    # Check if the URL is valid
                     response = requests.get(l[3])
                     if response.status_code == 200:
                         synset.url = l[3]
+                        # Lemmas and descriptions are not available in the WordNet gloss file, so scrape them from the HTML
                         if not synset.lemmas:
                             lemmas, description = self.find_lemmas_description_in_html(response.text)
                             synset.lemmas = lemmas
@@ -173,7 +191,6 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        import nltk
         nltk.download('wordnet')
         nltk.download('omw')
         self.download_links_csv()
