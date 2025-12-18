cade4e3dbcd7db46bf250b4778b2aa7a04254f48
susanodd <s.johanna.even@hetnet.nl>
2025-12-18 07:57:48 +0100

    #1657: Communication types in choices dictionary with code labels
    Render template preview in Admin for communication editing, initial draft

--- signbank/attachments/admin.py
+++ signbank/attachments/admin.py
@@ -1,13 +1,113 @@
-from django.contrib import admin 
-from signbank.attachments.models import Attachment, Communication
+from django.contrib import admin
+from django.db import models
+from django import forms
+from django.forms import Textarea
+from django.template.loader import render_to_string
+from django import template
+from django.template import Context, Template
+from django.utils.translation import  gettext_lazy as _, gettext
+from joblib.externals.cloudpickle import instance
+
+from signbank.attachments.models import Attachment, Communication, COMMUNICATION_TYPES
 
 
 class AttachmentAdmin(admin.ModelAdmin):
     list_display = ['file', 'date', 'uploader']
 
 
+class CommunicationAdminForm(forms.ModelForm):
+    label = forms.CharField(label=_("Description of type of communication"),
+                            widget=forms.Select(attrs={'class': 'form-control'},
+                                                choices=COMMUNICATION_TYPES),
+                            help_text="""Label without spaces""")
+
+    template_subject = forms.CharField(label=_("Subject (rendered)"),
+                                       widget=forms.Textarea(attrs={'cols': 60, 'rows': 1}))
+    subject = forms.CharField(label=_("Subject (template)"),
+                              widget=forms.Textarea(attrs={'cols': 60, 'rows': 1}))
+    template_text = forms.CharField(label=_("Text (rendered)"),
+                                    widget=forms.Textarea(attrs={'cols': 80, 'rows': 18}))
+    text = forms.CharField(label=_("Text (template"),
+                           widget=forms.Textarea(attrs={'cols': 80, 'rows': 18}))
+
+    class Meta:
+        model = Communication
+        fields = ['label', 'template_subject']
+
+    def template_context(self):
+        # template context with strings
+        placeholders = {'site_name': 'SITE NAME',
+                        'signbank_name': 'SIGNBANK',
+                        'url' : 'URL',
+                        'user' : {'firstname': 'FIRST NAME',
+                                  'lastname': 'LAST NAME',
+                                  'email': 'EMAIL',
+                                  'username': 'USERNAME'},
+                        'new_user_firstname': 'NEW USER FIRST NAME',
+                        'new_user_lastname': 'NEW USER LAST NAME',
+                        'new_user_email': 'NEW USER EMAIL',
+                        'dataset': 'DATASET',
+                        'motivation': 'MOTIVATION',
+                        'site': {'name': 'SITE NAME'}}
+        return Context(placeholders)
+
+    def __init__(self, *args, **kwargs):
+        super(CommunicationAdminForm, self).__init__(*args, **kwargs)
+        if not self.instance:
+            self.fields['label'].initial = '-'
+            self.fields['template_subject'].initial = "Email Subject"
+            self.fields['template_text'].initial = "Email Text"
+        else:
+            subject_template = Template(self.instance.subject)
+            self.fields['template_subject'].initial = subject_template.render(self.template_context())
+            self.fields['template_subject'].disabled = True
+            subject_template = Template(self.instance.text)
+            self.fields['template_text'].initial = subject_template.render(self.template_context())
+            self.fields['template_text'].disabled = True
+
+
 class CommunicationAdmin(admin.ModelAdmin):
-    list_display = ['label', 'subject', 'text']
+    form = CommunicationAdminForm
+
+    list_display = ['label', 'subject', 'subject_help', 'text', 'text_help']
+
+    class Media:
+        css = {
+            'all': ('css/custom_admin.css',)
+        }
+
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 60})}
+    }
+
+    def template_context(self):
+        # template context with strings
+        placeholders = {'site_name': 'SITE NAME',
+                        'signbank_name': 'SIGNBANK',
+                        'url' : 'URL',
+                        'user' : {'firstname': 'FIRST NAME',
+                                  'lastname': 'LAST NAME',
+                                  'email': 'EMAIL',
+                                  'username': 'USERNAME'},
+                        'new_user_firstname': 'NEW USER FIRST NAME',
+                        'new_user_lastname': 'NEW USER LAST NAME',
+                        'new_user_email': 'NEW USER EMAIL',
+                        'dataset': 'DATASET',
+                        'motivation': 'MOTIVATION',
+                        'site': {'name': 'SITE NAME'}}
+        return Context(placeholders)
+
+    admin.display(empty_value="EMPTY")
+    def subject_help(self, obj):
+        template = Template(obj.subject)
+        highlight = template.render(self.template_context())
+        return highlight
+
+    admin.display(empty_value="EMPTY")
+    def text_help(self, obj):
+        template = Template(obj.text)
+        highlight = template.render(self.template_context())
+        return highlight
 
 
 admin.site.register(Attachment, AttachmentAdmin)
--- signbank/attachments/models.py
+++ signbank/attachments/models.py
@@ -28,9 +28,20 @@ class Attachment(models.Model):
         return self.file.name
 
 
+COMMUNICATION_TYPES = [('-', 'Type of Communication'),
+                       ('activation_email', 'Mail to New User: Welcome to Signbank'),
+                       ('dataset_to_owner_existing_user_given_access', 'Mail to Dataset Owner: Existing User Given Access'),
+                       ('dataset_to_owner_new_user_given_access', 'Mail to Dataset Owner: New User Given Access'),
+                       ('dataset_to_owner_user_requested_access', 'Mail to Dataset Owner: User Requested Access'),
+                       ('dataset_to_user_existing_user_given_access', 'Mail to Existing User: Access Granted')]
+
+
 class Communication(models.Model):
-    label = models.CharField(_("Description of type of communication"), max_length=50, help_text="""Label without spaces""")
-    subject = models.TextField(_("Site communication subject"), help_text="""The communication text.""")
+    label = models.CharField(_("Description of type of communication"),
+                             max_length=50,
+                             choices=COMMUNICATION_TYPES,
+                             help_text="""Label without spaces""")
+    subject = models.TextField(_("Site communication subject"), help_text="""The communication subject.""")
     text = models.TextField(_("Site communication text"), help_text="""The communication text.""")
 
     class Meta:
