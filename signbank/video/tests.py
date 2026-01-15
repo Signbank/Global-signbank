829b2c396b99a53c8806b2571ef02efa5b7a1f1a
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-01-15 16:13:33 +0100

    Improve tests.py from 'video' app

--- signbank/video/tests.py
+++ signbank/video/tests.py
@@ -1,24 +1,14 @@
-from django.test import TestCase
+import os
 import unittest
-from unittest.mock import patch
-from django.core.files import File
+from io import StringIO
+
 from django.core.management import call_command
 from django.conf import settings
-from signbank.settings.base import *
-from signbank.video.models import GlossVideo
+
 from signbank.video.management.commands import remove_unused
-from io import StringIO
-import os
-import shutil
-import sys
 
 
 class CommandTests(unittest.TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        print("setting up - not used")
-
     def tearDown(self):
         """Remove all files added for test"""
         print("\n\nTeardown:")
@@ -53,71 +43,14 @@ class CommandTests(unittest.TestCase):
         some_folder_path = os.path.join(settings.WRITABLE_FOLDER, "somefolder")
         if not os.path.exists(some_folder_path):
             os.mkdir(some_folder_path)
-        glossvideofile = open(some_folder_path+os.sep+"fileNOTindb.txt", "a")    # file that is not in database
-        glossvideofile.close()
-        glossvideofile2 = open(some_folder_path+os.sep+"fileindb.txt", "a")      # file that is in database
-        glossvideofile2.close()
+        with open(os.path.join(some_folder_path, "fileNOTindb.txt"), 'a'):
+            pass
+        with open(os.path.join(some_folder_path, "fileindb.txt"), 'a'):
+            pass
 
         # files in database
         filenames_in_db = ['fileindb.txt']
 
         # compare test files with db filenames
         unused_files_found = remove_unused.find_unused_files(self, some_folder_path, filenames_in_db)
-
         self.assertEqual(unused_files_found, ["fileNOTindb.txt"])
-
-
-class VideoTests(unittest.TestCase):
-    # These tests are causing UnicodeDecodeError errors.
-    def setUp(self):
-        sys.stderr.write('inside of VideoTests setUp\n')
-        self.vidfilename = "signbank/video/testmedia/video.mp4"
-        self.videofile = File(open(self.vidfilename), "12345.mp4")
-
-    # def test_Video_create(self):
-    #     """We can create a video object"""
-    #     sys.stderr.write('inside of VideoTests test_Video_create\n')
-    #
-    #     vid = Video.objects.create(videofile=self.videofile)
-    #
-    #     # new file should be located in upload
-    #     self.assertEqual(os.path.dirname(vid.videofile.name), settings.VIDEO_UPLOAD_LOCATION)
-    #     self.assertTrue(os.path.exists(vid.videofile.path), "vidfile doesn't exist at %s" % (vid.videofile.path,))
-    #
-    #     # test deletion of files
-    #
-    #     vid.delete_files()
-    #     self.assertFalse(os.path.exists(vid.videofile.path), "vidfile still exists after delete at %s" % (vid.videofile.path,))
-    #
-    # def test_GlossVideo_create(self):
-    #     """We can create a GlossVideo object"""
-    #     sys.stderr.write('inside of VideoTests test_GlossVideo_create\n')
-    #
-    #     vid = GlossVideo.objects.create(videofile=self.videofile)
-    #
-    #
-    #     self.assertTrue(os.path.exists(vid.videofile.path), "vidfile doesn't exist at %s" % (vid.videofile.path,))
-    #
-    #     vid.delete_files()
-    #     self.assertFalse(os.path.exists(vid.videofile.path), "vidfile still exists after delete at %s" % (vid.videofile.path,))
-    #
-    #
-    #
-    # def test_Video_poster_path(self):
-    #     """We can generate a poster image for a video"""
-    #     sys.stderr.write('inside of VideoTests test_Video_poster_path\n')
-    #
-    #     vid = Video.objects.create(videofile=self.videofile)
-    #     # remove video file when done
-    #     self.addCleanup(lambda: os.unlink(vid.videofile.path))
-    #
-    #     poster = vid.poster_path()
-    #     poster_abs = os.path.join(settings.MEDIA_ROOT, poster)
-    #     # remove poster image when done
-    #     self.addCleanup(lambda: os.unlink(poster_abs))
-    #
-    #     self.assertTrue(os.path.exists(poster_abs), "poster image %s is missing" % (poster_abs,))
-    #
-    #     # do it again should give the same result, but won't have created the file
-    #     poster2 = vid.poster_path()
-    #     self.assertEqual(poster, poster2)
