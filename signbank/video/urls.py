3952ffe57d38ebd8741211a1852252d278730b04
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-01-15 16:17:26 +0100

    Improve urls.py from 'video' app

--- signbank/video/urls.py
+++ signbank/video/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import *
 from django.contrib.auth.decorators import permission_required
-from django.urls import re_path, path, include
+from django.urls import re_path
 
 import signbank.video.views
+
 
 urlpatterns = [
     re_path(r'^video/(?P<videoid>\d+)$', signbank.video.views.video),
@@ -10,5 +10,6 @@ urlpatterns = [
     re_path(r'^delete/(?P<glossid>\d+)$', signbank.video.views.deletevideo),
     re_path(r'^deletesentencevideo/(?P<videoid>\d+)$', signbank.video.views.deletesentencevideo),
     re_path(r'^process_eaffile/', signbank.video.views.process_eaffile, name='process_eaffile'),
-    re_path(r'^create_still_images/', permission_required('dictionary.change_gloss')(signbank.video.views.create_still_images))
-    ]
+    re_path(r'^create_still_images/',
+            permission_required('dictionary.change_gloss')(signbank.video.views.create_still_images))
+]
