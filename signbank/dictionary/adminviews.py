707c91351e751c1eaef27a96f779678de9cfddf0
susanodd <mijnoog@kpnmail.nl>
2026-01-22 13:56:15 +0100

    #1680: Gloss Videos button to remove duplicate vidoes
    that are backups but have the same checksum as the primary video

--- signbank/dictionary/adminviews.py
+++ signbank/dictionary/adminviews.py
@@ -56,7 +56,7 @@ from signbank.settings.server_specific import (URL, PREFIX_URL, LANGUAGE_CODE, L
 from signbank.video.forms import VideoUploadForObjectForm
 from signbank.video.convertvideo import get_folder_name
 from signbank.video.models import (GlossVideo, find_dangling_video_files, delete_glossvideo_objects_and_files,
-                                   renumber_backup_videos, remove_backup_videos)
+                                   renumber_backup_videos, remove_backup_videos, remove_duplicate_videos)
 from signbank.dictionary.models import (Dataset, UserProfile, AffiliatedUser, AffiliatedGloss,
                                         Language, Dialect, Gloss, Morpheme, GlossSense, Sense,
                                         Corpus, Speaker, Document, GlossFrequency,
@@ -1858,6 +1858,8 @@ class GlossVideosView(DetailView):
             return self.remove_backups(context)
         elif self.request.GET.get('operation') == 'Obsolete':
             return self.remove_obsolete(context)
+        elif self.request.GET.get('operation') == 'Duplicates':
+            return self.remove_duplicates(context)
         else:
             return super(GlossVideosView, self).render_to_response(context, **response_kwargs)
 
@@ -1883,6 +1885,14 @@ class GlossVideosView(DetailView):
                 and gloss.lemma.dataset in get_objects_for_user(self.request.user, ['change_dataset'], Dataset,
                                                                 any_perm=True)):
             delete_glossvideo_objects_and_files(gloss)
+        return HttpResponseRedirect(PREFIX_URL + '/dictionary/gloss/'+str(gloss.id)+'/glossvideos')
+
+    def remove_duplicates(self, context):
+        gloss = context['gloss']
+        if (self.request.user.has_perm('dictionary.change_gloss')
+                and gloss.lemma.dataset in get_objects_for_user(self.request.user, ['change_dataset'], Dataset,
+                                                                any_perm=True)):
+            remove_duplicate_videos(gloss)
         return HttpResponseRedirect(PREFIX_URL + '/dictionary/gloss/'+str(gloss.id)+'/glossvideos')
 
 
