d90098a0051ac58a32b5de74701b76cfcde773d7
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-02-03 13:55:42 +0100

    Improve command make_missing_gloss_images from 'video' app

--- signbank/video/management/commands/make_missing_gloss_images.py
+++ signbank/video/management/commands/make_missing_gloss_images.py
@@ -1,15 +1,16 @@
 """Make missing (thumbnail) images for existing videos"""
 
+import os
 import ffmpeg
 
 from django.core.management.base import BaseCommand  
 from django.conf import settings
+
 from signbank.video.models import GlossVideo
 from signbank.video.convertvideo import get_middle_timecode
-import os
+
 
 class Command(BaseCommand):
-     
     help = 'Create missing PNG images for all videos'
 
     def splitext_recurse(self, path):
@@ -17,51 +18,37 @@ class Command(BaseCommand):
         base, ext = os.path.splitext(path)
         if ext == '':
             return base
-        else:
-            return self.splitext_recurse(base)
+        return self.splitext_recurse(base)
 
     def handle(self, *args, **options):
-        
-        images_created = []
-        for vid in GlossVideo.objects.all():
+        images_created = [
+            created_image for gloss_video in GlossVideo.objects.all()
+            if os.path.exists(gloss_video.videofile.path)
+               and (created_image := self.create_image(gloss_video))
+        ]
 
-            video_path = vid.videofile.path
-
-            # if the videofile for this gloss exists
-            if not os.path.exists(video_path):
-                continue
-                
-            # Make the expected image path
-            image_path = self.splitext_recurse(video_path) + ".png"
-            image_path = str(image_path.replace(settings.GLOSS_VIDEO_DIRECTORY, settings.GLOSS_IMAGE_DIRECTORY, 1))
-
-            # Check if it does not have an image
-            if not os.path.exists(image_path):
-                
-                # If a folder for the image does not exist yet, add one
-                image_dir = os.path.dirname(image_path)
-                if not os.path.isdir(image_dir):
-                    os.makedirs(image_dir)
-
-                try:
-                    # make the missing image from the middle frame of the video
-                    (ffmpeg
-                     .input(video_path)
-                     .output(image_path, r=1, f="mjpeg", ss=get_middle_timecode(video_path))
-                     .run(overwrite_output=True, quiet=True))
-                    images_created.append(os.path.basename(image_path))
-                except:
-                    print("Error in creating an image for the following video: ", os.path.basename(video_path))
-
-        # Feedback about which images were created
         if len(images_created) == 0:
             print("No new images were added")
         else:
-            print("The following images were added: ")
-            for im in images_created:
-                print(im)
-        
+            print("The following images were added:")
+            print("\n".join(images_created))
 
-    
-
+    def create_image(self, gloss_video):
+        video_path = gloss_video.videofile.path
+        image_path = self.splitext_recurse(video_path).replace(settings.GLOSS_VIDEO_DIRECTORY,
+                                                               settings.GLOSS_IMAGE_DIRECTORY, 1) + ".png"
+        if os.path.exists(image_path):
+            return None
+        image_dir = os.path.dirname(image_path)
+        if not os.path.isdir(image_dir):
+            os.makedirs(image_dir)
+        try:
+            (ffmpeg
+             .input(video_path)
+             .output(image_path, r=1, f="mjpeg", ss=get_middle_timecode(video_path))
+             .run(overwrite_output=True, quiet=True))
+            return os.path.basename(image_path)
+        except ffmpeg.Error:
+            print("Error in creating an image for the following video: ", os.path.basename(video_path))
+            return None
     
