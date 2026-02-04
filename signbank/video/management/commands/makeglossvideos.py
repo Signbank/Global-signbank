3a39ea4b3050ed73679bb8f57ab2220a1092212a
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-02-04 11:52:23 +0100

    Improve command makeglossvideos from 'video' app

--- signbank/video/management/commands/makeglossvideos.py
+++ signbank/video/management/commands/makeglossvideos.py
@@ -1,19 +1,24 @@
+import os
+from itertools import chain
+import logging
+
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.conf import settings
 from django.db.models.query import QuerySet
+from django.utils.encoding import escape_uri_path
+
 from signbank.dictionary.models import Gloss
 from signbank.video.models import GlossVideo
 
-import os
 
-from itertools import chain
+logger = logging.getLogger(__name__)
 
 
-def parse_range(rng):
-    parts = rng.split('-')
-    if 1 > len(parts) > 2:
-        raise ValueError("Bad range: '%s'" % (rng,))
+def parse_range(range_to_parse):
+    parts = range_to_parse.split('-')
+    if not 1 <= len(parts) <= 2:
+        raise ValueError("Bad range: '%s'" % (range_to_parse,))
     parts = [int(i) for i in parts]
     start = parts[0]
     end = start if len(parts) == 1 else parts[1]
@@ -22,8 +27,8 @@ def parse_range(rng):
     return range(start, end + 1)
 
 
-def parse_range_list(rngs):
-    return sorted(set(chain(*[parse_range(rng) for rng in rngs.split(',') if rng])))
+def parse_range_list(ranges):
+    return sorted(set(chain(*[parse_range(range) for range in ranges.split(',') if range])))
 
 
 class Command(BaseCommand):
@@ -31,19 +36,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # Named (optional) arguments
-        parser.add_argument(
-            '--dry-run',
-            action='store_true',
-            dest='dry-run',
-            default=False,
-            help='Make a dry run, i.e. do not make changes to the database or move files.',
-        )
-        parser.add_argument(
-            '--gloss-ids',
-            dest='gloss-ids',
-            default='',
-            help='Specify the gloss IDs [,]',
-        )
+        parser.add_argument('--dry-run', action='store_true', dest='dry-run', default=False,
+                            help='Make a dry run, i.e. do not make changes to the database or move files.',)
+        parser.add_argument('--gloss-ids', dest='gloss-ids', default='',
+                            help='Specify the gloss IDs [,]',)
 
     def handle(self, *args, **options):
         gloss_ids = parse_range_list(options['gloss-ids'])
@@ -52,113 +48,99 @@ class Command(BaseCommand):
 
 def get_contructed_video(gloss):
     """Get the video path from idgloss"""
-
     foldername = gloss.idgloss[:2]
     if len(foldername) == 1:
         foldername += '-'
 
-    constructed_video_path = os.path.join('glossvideo', foldername,
-                                          gloss.idgloss + '-' + str(gloss.pk) + '.mp4')
+    constructed_video_path = os.path.join('glossvideo', foldername, gloss.idgloss + '-' + str(gloss.pk) + '.mp4')
     if hasattr(settings, 'ESCAPE_UPLOADED_VIDEO_FILE_PATH') and settings.ESCAPE_UPLOADED_VIDEO_FILE_PATH:
-        from django.utils.encoding import escape_uri_path
         constructed_video_path = escape_uri_path(constructed_video_path)
 
-    constructed_video_exists = os.path.exists(os.path.join(settings.WRITABLE_FOLDER,
-                                                           constructed_video_path))
-    print("Gloss {} (id: {}): Constructed video path: {}".format(gloss, gloss.id, constructed_video_path))
+    logger.info(f"Gloss {gloss} (id: {gloss.id}): Constructed video path: {constructed_video_path}")
+    constructed_video_exists = os.path.exists(os.path.join(settings.WRITABLE_FOLDER, constructed_video_path))
     if constructed_video_exists:
-        print("Gloss {} (id: {}): Constructed video exists".format(gloss, gloss.id, constructed_video_path))
+        logger.info(f"Gloss {gloss} (id: {gloss.id}): Constructed video exists")
 
     return constructed_video_path, constructed_video_exists
 
 
 def get_glossvideo_path(gloss, dry_run):
     """Get the video path from GlossVideo"""
-    glossvideo = None
-    glossvideo_video_path = None
-    glossvideo_video_exists = None
     try:
         glossvideo = gloss.glossvideo_set.get(version=0)
         glossvideo_video_path = glossvideo.videofile.name
         glossvideo_video_exists = os.path.exists(os.path.join(settings.WRITABLE_FOLDER, glossvideo_video_path))
+        return glossvideo, glossvideo_video_path, glossvideo_video_exists
     except ObjectDoesNotExist:
-        pass
+        return None, None, None
     except MultipleObjectsReturned:
-        glossvideo_set = GlossVideo.objects.filter(gloss=gloss, version=0).order_by('-id')
-        glossvideofile_set = set([gv.videofile.name for gv in glossvideo_set])
-        if len(glossvideofile_set) == 1:
-            if dry_run:
-                print("Gloss {} (id: {}): multiple GlossVideo objects with version 0, all the same"
-                      .format(gloss, gloss.pk))
-            else:
-                glossvideo = glossvideo_set[0]
-                glossvideo_video_path = glossvideo.videofile.name
-                glossvideo_video_exists = os.path.exists(os.path.join(settings.WRITABLE_FOLDER, glossvideo_video_path))
-                glossvideo_set.exclude(id=glossvideo.id).delete()
-        else:
-            print("Gloss {} (id: {}): multiple GlossVideo objects with version 0"
-                  .format(gloss, gloss.pk))
-            glossvideo = glossvideo_set
+        return get_path_for_multiple_glossvideos(gloss, dry_run)
 
+
+def get_path_for_multiple_glossvideos(gloss, dry_run, glossvideo=None, glossvideo_video_path=None,
+                                      glossvideo_video_exists=None):
+    glossvideo_set = GlossVideo.objects.filter(gloss=gloss, version=0).order_by('-id')
+    glossvideofile_set = {gloss_video.videofile.name for gloss_video in glossvideo_set}
+    if len(glossvideofile_set) != 1:
+        logger.info(f"Gloss {gloss} (id: {gloss.pk}): multiple GlossVideo objects with version 0")
+        glossvideo = glossvideo_set
+        return glossvideo, glossvideo_video_path, glossvideo_video_exists
+    if dry_run:
+        logger.info(f"Gloss {gloss} (id: {gloss.pk}): multiple GlossVideo objects with version 0, all the same")
+        return glossvideo, glossvideo_video_path, glossvideo_video_exists
+    glossvideo = glossvideo_set[0]
+    glossvideo_video_path = glossvideo.videofile.name
+    glossvideo_video_exists = os.path.exists(os.path.join(settings.WRITABLE_FOLDER, glossvideo_video_path))
+    glossvideo_set.exclude(id=glossvideo.id).delete()
     return glossvideo, glossvideo_video_path, glossvideo_video_exists
 
 
 def create_new_glossvideo(gloss, video_path, dry_run):
     # Backup the existing video objects stored in the database
     if not dry_run:
-        existing_videos = GlossVideo.objects.filter(gloss=gloss)
-        for video_object in existing_videos:
+        for video_object in GlossVideo.objects.filter(gloss=gloss):
             video_object.reversion(revert=False)
 
-    print("Gloss {} (id: {}): creating a new glossvideo.".format(gloss, gloss.id))
+    logger.info(f"Gloss {gloss} (id: {gloss.id}): creating a new glossvideo.")
     new_glossvideo = GlossVideo(gloss=gloss, videofile=video_path)
     if not dry_run:
         new_glossvideo.save()
         new_glossvideo.move_video()
 
 
-def make_gloss_videos(dry_run=False, gloss_ids=[]):
-    if gloss_ids:
-        glosses = Gloss.objects.filter(id__in=gloss_ids)
-    else:
-        glosses = Gloss.objects.all()
-    for gloss in glosses:
+def make_gloss_videos(dry_run=False, gloss_ids=None):
+    for gloss in Gloss.objects.filter(id__in=gloss_ids or []) if gloss_ids else Gloss.objects.all():
         constructed_video_path, constructed_video_exists = get_contructed_video(gloss)
         glossvideo, glossvideo_video_path, glossvideo_video_exists = get_glossvideo_path(gloss, dry_run)
 
-        # Go through all possibilities
-        if constructed_video_exists:
-            if isinstance(glossvideo, GlossVideo):
-                if glossvideo_video_path == constructed_video_path:
-                    print("Gloss {} (id: {}): the glossvideo path and constructed path are the same".format(gloss, gloss.id))
-                    print("Gloss {} (id: {}): the video file is moved".format(gloss, gloss.id))
-                    if not dry_run:
-                        glossvideo.move_video()
-                else:
-                    if glossvideo_video_exists:
-                        print("Gloss {} (id: {}): the constructed path ({}) and the glossvideo path ({}) are not "
-                              "equal and both refer to an existing file"
-                              .format(gloss, gloss.id, constructed_video_path, glossvideo_video_path))
-                    else:
-                        print("Gloss {} (id: {}): the existing glossvideo gets the constructed path"
-                              .format(gloss, gloss.id))
-                        glossvideo.videofile.name = constructed_video_path
-                        print("Gloss {} (id: {}): the video file is moved".format(gloss, gloss.id))
-                        if not dry_run:
-                            glossvideo.save()
-                            glossvideo.move_video()
-            elif isinstance(glossvideo, QuerySet):
+        if not constructed_video_exists:
+            if isinstance(glossvideo, QuerySet):
+                pass  # TODO handle multiple different glossvideos
+            elif glossvideo and not glossvideo_video_exists:
+                glossvideo.delete() if not dry_run else None
+            elif glossvideo:
+                logger.info(f"Gloss {gloss} (id: {gloss.id}): the video file is moved")
+                glossvideo.move_video() if not dry_run else None
+
+        elif not isinstance(glossvideo, GlossVideo):
+            if isinstance(glossvideo, QuerySet):
                 pass  # TODO handle multiple different glossvideos
             else:
                 create_new_glossvideo(gloss, constructed_video_path, dry_run=dry_run)
+
+        elif glossvideo_video_path == constructed_video_path:
+            logger.info(f"Gloss {gloss} (id: {gloss.id}): the glossvideo path and constructed path are the same")
+            logger.info("Gloss {gloss} (id: {gloss.id}): the video file is moved")
+            glossvideo.move_video() if not dry_run else None
+
+        elif glossvideo_video_exists:
+            logger.info(f"Gloss {gloss} (id: {gloss.id}): the constructed path ({constructed_video_path}) "
+                        f"and the glossvideo path ({glossvideo_video_path}) are not equal and both refer "
+                        f"to an existing file")
+
         else:
-            if glossvideo:
-                if glossvideo_video_exists:
-                    print("Gloss {} (id: {}): the video file is moved".format(gloss, gloss.id))
-                    if not dry_run:
-                        glossvideo.move_video()
-                else:
-                    if not dry_run:
-                        glossvideo.delete()
-            elif isinstance(glossvideo, QuerySet):
-                pass  # TODO handle multiple different glossvideos
+            logger.info(f"Gloss {gloss} (id: {gloss.id}): the existing glossvideo gets the constructed path")
+            glossvideo.videofile.name = constructed_video_path
+            logger.info(f"Gloss {gloss} (id: {gloss.id}): the video file is moved")
+            glossvideo.save() if not dry_run else None
+            glossvideo.move_video() if not dry_run else None
