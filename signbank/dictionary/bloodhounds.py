5413eac11de47ab739ec1d11ab1ef35a1a595a0e
susanodd <mijnoog@kpnmail.nl>
2026-04-02 11:25:12 +0200

    #1714: Separate file for bloodhounds

---
+++ signbank/dictionary/bloodhounds.py
@@ -0,0 +1,27 @@
+
+from django.http import JsonResponse
+
+from signbank.dictionary.models import FieldChoice, Gloss, Handshape
+
+# def handshape_ajax_complete(request, prefix):
+#     """Return a list of handshapes matching the search term
+#     as a JSON structure suitable for typeahead."""
+#     qs = Handshape.objects.filter(name__istartswith=prefix)
+#
+#     result = []
+#     for g in qs:
+#         result.append({'name': g.name, 'machine_value': g.machine_value})
+#
+#     return JsonResponse(result, safe=False)
+
+
+def handedness_ajax_complete(request, prefix):
+    """Return a list of field choices matching the search term"""
+    qs = FieldChoice.objects.filter(field='Handedness', name__istartswith=prefix)
+
+    result = []
+    for f in qs:
+        result.append({'name': f.name, 'machine_value': f.machine_value})
+
+    return JsonResponse(result, safe=False)
+
