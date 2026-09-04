da78bf1f0bff847e8ec0f07c8144646eb08bcde4
Wessel Stoop <woseseltops@hotmail.com>
2026-09-04 16:10:16 +0200

    Removed tagging reference from tools.py #1775

--- signbank/tags/migrations/0002_migrate_from_tagging.py
+++ signbank/tags/migrations/0002_migrate_from_tagging.py
@@ -4,44 +4,58 @@ from django.db import migrations
 from django.contrib.contenttypes.models import ContentType
 
 
+# Data migration to migrate from django-tagging to custom tags
+
+from django.db import migrations
+from django.contrib.contenttypes.models import ContentType
+
+
 def migrate_from_tagging(apps, schema_editor):
     """Migrate existing tags from django-tagging to custom tags app"""
     Tag = apps.get_model('tags', 'Tag')
     TaggedItem = apps.get_model('tags', 'TaggedItem')
     
-    # Try to import django-tagging models
+    # Try to import django-tagging models if package is still installed
     try:
         from tagging.models import Tag as OldTag, TaggedItem as OldTaggedItem
-    except ImportError:
-        # django-tagging not installed, skip migration
+        old_tagging_available = True
+    except (ImportError, ModuleNotFoundError, RuntimeError):
+        # django-tagging not installed or not in INSTALLED_APPS, skip migration
         return
     
-    # Get the Gloss content type from the old system
+    if not old_tagging_available:
+        return
+    
+    # Get the Gloss content type
     try:
         from signbank.dictionary.models import Gloss
         gloss_ct = ContentType.objects.get_for_model(Gloss)
-    except:
+    except Exception:
         return
     
-    # Migrate all old tags
-    old_tags = OldTag.objects.all()
-    for old_tag in old_tags:
-        # Create or get the new tag with the same name
-        new_tag, _ = Tag.objects.get_or_create(name=old_tag.name)
-        
-        # Get all tagged items for this old tag
-        old_items = OldTaggedItem.objects.filter(tag=old_tag)
-        
-        for old_item in old_items:
-            # Only migrate Gloss tags (object_id and content_type_id match Gloss)
-            if old_item.content_type.app_label == 'dictionary' and old_item.content_type.model == 'gloss':
-                # Create the new TaggedItem
-                TaggedItem.objects.get_or_create(
-                    tag=new_tag,
-                    content_type=gloss_ct,
-                    object_id=old_item.object_id,
-                    defaults={'created': old_item.created}
-                )
+    try:
+        # Migrate all old tags
+        old_tags = OldTag.objects.all()
+        for old_tag in old_tags:
+            # Create or get the new tag with the same name
+            new_tag, _ = Tag.objects.get_or_create(name=old_tag.name)
+            
+            # Get all tagged items for this old tag
+            old_items = OldTaggedItem.objects.filter(tag=old_tag)
+            
+            for old_item in old_items:
+                # Only migrate Gloss tags (object_id and content_type_id match Gloss)
+                if old_item.content_type.app_label == 'dictionary' and old_item.content_type.model == 'gloss':
+                    # Create the new TaggedItem
+                    TaggedItem.objects.get_or_create(
+                        tag=new_tag,
+                        content_type=gloss_ct,
+                        object_id=old_item.object_id,
+                        defaults={'created': old_item.created}
+                    )
+    except Exception:
+        # If anything fails, just continue - the migration is best-effort
+        pass
 
 
 def reverse_migrate(apps, schema_editor):
